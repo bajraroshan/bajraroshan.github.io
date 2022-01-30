de30528791b525278a251e703082f01221f66c24
bajraroshan <bajraroshan@gmail.com>
2022-01-30 22:57:08 +1030

    hamburger menu implemented

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,25 @@
-jQuery('.slick').slick({
+$(document).ready(function () {
+  $(".hamburger-icon").on( "click", function() {
+    $(this).toggleClass("open", 1000);
+    $(this).parent().toggleClass("mobilenav-open", 1000);
+  });
+
+  function revoke(){
+    if ($(window).width() < 767) {
+      $('nav ul li a').on( "click", function() {
+        $(this).parents("div").removeClass("open", 1000);
+        $(this).parents("nav").removeClass("mobilenav-open", 1000);
+      });
+    }
+  } 
+
+  revoke();
+
+  $(window).resize(function () {
+    revoke();
+  });
+
+  jQuery(".slick").slick({
     dots: true,
     infinite: true,
     speed: 300,
@@ -9,18 +30,16 @@ jQuery('.slick').slick({
         breakpoint: 767,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 481,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
+});
